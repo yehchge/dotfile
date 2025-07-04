e8a504d1a1dc32a97591bc5ed4e3a1cdfebf2854
bill.yeh <bill.yeh@iwant-in.net>
2025-07-04 10:09:53 +0800

    update switch-php path

--- app/morning.sh
+++ app/morning.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
-./switch-php.sh 8.3
-sudo service apache2 start
+~/app/switch-php.sh 8.3
+sudo service apache2 restart
 #~/app/set_samba.sh
